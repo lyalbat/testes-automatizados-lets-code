dddfb70688ba21ce1aa3cbef6790a2a3cb741873
lyalbat <dwlary@hotmail.com>
2022-12-08 20:14:46 -0300

    adicionando novos testes controller

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const server = require('./src/app')
 
-mongoose.connect('mongodb+srv://larissa:senha@testes.zoe9u2q.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://letscode:letscode@cluster0.nwudzbw.mongodb.net/letscode?retryWrites=true&w=majority')
     .then(() =>{
         console.log('Mongo connected')
         server.listen(3000, () => console.log('Server listening on PORT:::3000'))
--- tests/units/controllers/user-ctrl.spec.js
+++ tests/units/controllers/user-ctrl.spec.js
@@ -2,40 +2,60 @@ const UserController = require('../../../src/controllers/user-ctrl')
 const UserService = require('../../../src/services/user-service')
 const { getReqMock, getResMock, getResponses } = require('../../mocks/session-mocks')
 
-class UsersMock {
-    /*static async userExists(email) {        
-        if(email === 'valid.email@lets.com.br') {
+class UserServiceMock {
+    static async userExists(user) {        
+        if(user === 'letscode') {
             return true
         } else {
             return false
         }
     }
 
-    static async checkPassword(email, password) {
+    static async checkPassword(user, password) {
         return password === '123456'
     }
-    */
 }
 
-describe('UserController create', () => {
-    it('Should return status 400 if no email is provided', async () => {
-        const req = getReqMock()
+describe('User Controller', () => {
+    it('Should return status 200 if user is provided', async () => {
+        const req = getReqMock({
+            "name": "letscode",
+            "email": "letscode@letscode.com.br",
+            "password": "123456"
+          })
 
         const res = getResMock()
-        const { invalidEmail } = getResponses()   
+        const { invalidName } = getResponses()   
 
+        jest.spyOn(UserService, 'create').mockImplementationOnce(() => {
+            return {
+                "name": "letscode",
+                "email": "letscode@letscode.com.br",
+                "password": "123456",
+                "_id": "638e7d723af54a8824c5a77d",
+                "__v": 0
+              }
+        })
         const response = await UserController.create(req, res)
 
-        expect(response.status).toBe(400)
-        expect(response.data).toMatchObject(invalidEmail)        
+        expect(response.status).toBe(200)
+        expect(response.data).toMatchObject({
+            "user": {
+                "name": "letscode",
+                "email": "letscode@letscode.com.br",
+                "password": "123456",
+                "_id": "638e7d723af54a8824c5a77d",
+                "__v": 0
+              }
+        })        
     })
 
-    it('Should return status 400 if an invalid email is provided', async () => {
-        const req = getReqMock({ email: 'esdras.com.br'})
+    it('Should return status 400 if email is not provided or invalid', async () => {
+        const req = getReqMock({ name: 'letscode' })
 
         const res = getResMock()
         const { invalidEmail } = getResponses()   
-
+       
         const response = await UserController.create(req, res)
 
         expect(response.status).toBe(400)
@@ -43,7 +63,7 @@ describe('UserController create', () => {
     })
 
     it('Should return status 400 if no password is provided', async () => {
-        const req = getReqMock({ email: 'esdras@lets.com.br'})
+        const req = getReqMock({ email: 'letscode@letscode.com.br' })
 
         const res = getResMock()
         const { invalidPassword } = getResponses()   
@@ -53,47 +73,167 @@ describe('UserController create', () => {
         expect(response.status).toBe(400)
         expect(response.data).toMatchObject(invalidPassword)        
     })
+})
 
-    /*it('Should return status 401 if user does not exist', async () => {
-        jest.spyOn(UserService, 'userExists').mockImplementationOnce(UserServiceMock.userExists)
+/*static async changePassword(req, res) {
+        try {
+            const { email, oldPassword, newPassword, confirmPassword} = req.body
 
-        const req = getReqMock({ email: 'esdras@lets.com.br', password: '123456' })
+            await UserService.updatePassword(email, newPassword, confirmPassword)
+    
+            return res.status(200).json({
+                message: 'ok'
+            })
+        } catch (error) {
+            return res.status(error.status || 500).json({ message: error.message })
+        }*/
+
+describe('User Controller changePassword', () => {
+    beforeEach(() => {
+        jest.setTimeout(60000);
+      })
+    it('Should return status 400 if email is not provided', async () => {
+        const req = getReqMock({
+            "email": "",
+            "oldPassword": "123456",
+            "newPassword": "123457",
+            "confirmPassword":"123457"
+        })
 
         const res = getResMock()
-        const { invalidCredentials } = getResponses()   
+        const { invalidEmail } = getResponses()   
 
-        const response = await SessionController.create(req, res)
+        const response = await UserController.changePassword(req, res)
 
-        expect(response.status).toBe(401)
-        expect(response.data).toMatchObject(invalidCredentials)        
+        expect(response.status).toBe(400)
+        expect(response.data).toMatchObject(invalidEmail)       
     })
-
-    it('Should return status 401 if password does not match', async () => {
-        jest.spyOn(UserService, 'userExists').mockImplementationOnce(UserServiceMock.userExists)
-        jest.spyOn(UserService, 'checkPassword').mockImplementationOnce(UserServiceMock.checkPassword)
-
-        const req = getReqMock({ email: 'valid.email@lets.com.br', password: '123455' })
+    //se der erro, reze e volte o teste tres vezes!
+    it("should return status 400 if email is invalid", async () => {
+        const req = getReqMock({
+            "email": "email.com",
+            "oldPassword": "123456",
+            "newPassword": "123457",
+            "confirmPassword":"123457"
+        })
 
         const res = getResMock()
-        const { invalidCredentials } = getResponses()   
+        const { invalidEmail } = getResponses()   
 
-        const response = await SessionController.create(req, res)
+        const response = await UserController.changePassword(req, res)
 
-        expect(response.status).toBe(401)
-        expect(response.data).toMatchObject(invalidCredentials)        
+        expect(response.status).toBe(400)
+        expect(response.data).toMatchObject(invalidEmail)       
     })
-
-    it('Should return status 200 if email and password is valid', async () => {
-        jest.spyOn(UserService, 'userExists').mockImplementationOnce(UserServiceMock.userExists)
-        jest.spyOn(UserService, 'checkPassword').mockImplementationOnce(UserServiceMock.checkPassword)
-
-        const req = getReqMock({ email: 'valid.email@lets.com.br', password: '123456' })
+    it("should return status 400 if all passwords are not provided",async () =>{
+        const req = getReqMock({
+            "email": "email@email.com",
+            "oldPassword": "",
+            "newPassword": "",
+            "confirmPassword":""
+        })
 
         const res = getResMock()
+        const { invalidPassword } = getResponses()   
 
-        const response = await SessionController.create(req, res)
+        const response = await UserController.changePassword(req, res)
+
+        expect(response.status).toBe(400)
+        expect(response.data).toMatchObject(invalidPassword)     
+    })
+    it("should return status 400 if oldPassword is not provided",async () =>{
+        const req = getReqMock({
+            "email": "email@email.com",
+            "oldPassword": "",
+            "newPassword": "1245",
+            "confirmPassword":"1245"
+        })
+
+        const res = getResMock()
+        const { invalidPassword } = getResponses()   
+
+        const response = await UserController.changePassword(req, res)
+
+        expect(response.status).toBe(400)
+        expect(response.data).toMatchObject(invalidPassword)     
+    })
+    it("should return status 400 if newPassword is not provided",async () =>{
+        const req = getReqMock({
+            "email": "email@email.com",
+            "oldPassword": "1247",
+            "newPassword": "",
+            "confirmPassword":"1245"
+        })
+        
+        const res = getResMock()
+        const { invalidPassword } = getResponses()   
+
+        const response = await UserController.changePassword(req, res)
+
+        expect(response.status).toBe(400)
+        expect(response.data).toMatchObject(invalidPassword)     
+    })
+    it("should return status 400 if confirmPassword is not provided",async () =>{
+        const req = getReqMock({
+            "email": "email@email.com",
+            "oldPassword": "1247",
+            "newPassword": "1245",
+            "confirmPassword":"1245"
+        })
+
+        const res = getResMock()
+        const { invalidPassword } = getResponses()   
+
+        const response = await UserController.changePassword(req, res)
 
         expect(response.status).toBe(200)
-        expect(response.data).toHaveProperty('token')        
-    })*/
+        expect(response.data).toMatchObject(invalidPassword)     
+    })
+    it("should return 401 if oldPassword is wrong", async () =>{
+        const req = getReqMock({
+            "email": "email@email.com",
+            "oldPassword": "1247",
+            "newPassword": "1245",
+            "confirmPassword":"1245"
+        })
+
+        const res = getResMock()
+        const { invalidCredentials } = getResponses()   
+        
+        jest.spyOn(UserService,"checkPassword").mockImplementationOnce(()=>false)
+        
+        const response = await UserController.changePassword(req, res)
+
+        expect(response.status).toBe(401)
+        expect(response.data).toMatchObject(invalidCredentials)
+
+    })
+    it("should return 200 if the password was updated", async () =>{
+        const req = getReqMock({
+            "email": "email@email.com",
+            "oldPassword": "1247",
+            "newPassword": "1245",
+            "confirmPassword":"1245"
+        })
+
+        const res = getResMock()
+        
+        jest.spyOn(UserService, "checkPassword").mockImplementationOnce(()=> true)
+        
+        jest.spyOn(UserService, "updatePassword").mockImplementationOnce(()=> {
+            return {
+                "name": "nome",
+                "email": "email@email.com",
+                "password": "1245",
+                "_id": "638e76e00034058f1c083a19",
+                "__v": 0
+            }
+        })
+
+        const response = await UserController.changePassword(req, res)
+        
+        expect(response.status).toBe(200)
+        expect(response.data).toMatchObject({"message": "ok"})
+
+    })
 })
